c5823ab03b63e38021d4b7f63d01e59557ae5bf0
Manuel <160016796+Manuel-agd@users.noreply.github.com>
2025-02-11 12:20:53 +0100

    Actualizacion de la logica y UI

--- src/motor.ts
+++ src/motor.ts
@@ -51,53 +51,28 @@ export const sonPareja = (indiceA: number, indiceB: number, tablero: Tablero): b
 export const parejaEncontrada = (indiceA: number, indiceB: number, tablero: Tablero): void => {
     tablero.cartas[indiceA].emparejada = true;
     tablero.cartas[indiceB].emparejada = true;
+    tablero.cartas[indiceA].estaVolteada = true;
+    tablero.cartas[indiceB].estaVolteada = true;
     tablero.indiceCartaVolteadaA = undefined;
     tablero.indiceCartaVolteadaB = undefined;
     tablero.estadoPartida = "ceroCartasVolteadas";
 };
 
 export const parejaNoEncontrada = (tablero: Tablero, indiceA: number, indiceB: number): void => {
+    setTimeout(() => {
+    tablero.cartas[indiceA].emparejada = false;
+    tablero.cartas[indiceB].emparejada = false;
+    tablero.cartas[indiceA].estaVolteada = false;
+    tablero.cartas[indiceB].estaVolteada = false;
+    tablero.indiceCartaVolteadaA = undefined;
+    tablero.indiceCartaVolteadaB = undefined;
+    tablero.estadoPartida = "ceroCartasVolteadas";
     
-        console.log("Volteando las cartas incorrectas");
-    
-        tablero.cartas[indiceA].estaVolteada = false;
-        tablero.cartas[indiceB].estaVolteada = false;
-        tablero.indiceCartaVolteadaA = undefined;
-        tablero.indiceCartaVolteadaB = undefined;
-        tablero.estadoPartida = "ceroCartasVolteadas";
-
-        console.log("Volteando las cartas nuevamente");
-
-    // setTimeout(() => {
-    //     tablero.cartas[indiceA].estaVolteada = false;
-    //     tablero.cartas[indiceB].estaVolteada = false;
-
-    //     const divCartaA = document.querySelector(`#carta-${indiceA}`) as HTMLElement;
-    //     const divCartaB = document.querySelector(`#carta-${indiceB}`) as HTMLElement;
-
-    //     if (divCartaA) {
-    //         actualizarUIcarta(divCartaA, tablero.cartas[indiceA]);
-    //     } else {
-    //         console.warn("Elemento DOM para la carta-${indiceA} no encontrado")
-    //     }
-    //     if (divCartaB) {
-    //         actualizarUIcarta(divCartaB, tablero.cartas[indiceB]);
-    //     } else {
-    //         console.warn("Elemento DOM para la carta-${indiceB} no encontrado")
-    //     }
-
-    //     tablero.indiceCartaVolteadaA = undefined;
-    //     tablero.indiceCartaVolteadaB = undefined;
-    //     tablero.estadoPartida = "ceroCartasVolteadas";
-    // }, 1000)
+    console.log("Volteando las cartas nuevamente");
+    }, 1000);
 };
 
-export const voltearCarta = (tablero: Tablero, indice: number) : void => {
-    /*if (!sePuedeVoltearCarta(tablero, indice)) {
-        console.log("No puedes voltear esta carta");
-        return;
-    }*/ //Esto deberia estar en UI
-
+export const voltearCarta = (tablero: Tablero, indice: number): void => {
     tablero.cartas[indice].estaVolteada = true;
 
     if (tablero.estadoPartida === 'ceroCartasVolteadas') {
@@ -106,35 +81,7 @@ export const voltearCarta = (tablero: Tablero, indice: number) : void => {
     } else if (tablero.estadoPartida === "unaCartaVolteada") {
         tablero.indiceCartaVolteadaB = indice;
         tablero.estadoPartida = "dosCartasVolteadas";
-       // actualizarEstadoTablero(tablero, actualizarIntentos);
     }
-};
-
-export const actualizarEstadoTablero = (tablero: Tablero, actualizarIntentos: (intentos: number) => void): void => {
-    /*if (tablero.estadoPartida === "ceroCartasVolteadas") {
-        tablero.estadoPartida = "unaCartaVolteada";
-        tablero.indiceCartaVolteadaA = indice;
-    } else if (tablero.estadoPartida === "unaCartaVolteada") {
-        tablero.estadoPartida = "dosCartasVolteadas";
-        tablero.indiceCartaVolteadaB = indice;
-    }*/ 
-
-    if (tablero.estadoPartida === "dosCartasVolteadas") return;
-        const indiceA = tablero.indiceCartaVolteadaA!;
-        const indiceB = tablero.indiceCartaVolteadaB!;
-
-        tablero.intentos += 1;
-        actualizarIntentos(tablero.intentos);
-
-        if (sonPareja(indiceA, indiceB, tablero)) {
-            parejaEncontrada(indiceA, indiceB, tablero);
-        if (esPartidaCompleta(tablero)) {
-            tablero.estadoPartida = "partidaCompleta";
-            console.log("¡Partida Completa!");
-            }
-        } else {
-            parejaNoEncontrada(tablero, indiceA, indiceB);
-        }
 };
 
 export const esPartidaCompleta = (tablero: Tablero): boolean => {
--- src/ui.ts
+++ src/ui.ts
@@ -1,103 +1,102 @@
-import { Tablero, Carta, tablero } from "./modelo";
+import { Tablero, Carta } from "./modelo";
 import { esPartidaCompleta, inicializarJuego, parejaEncontrada, parejaNoEncontrada, sePuedeVoltearCarta, sonPareja, voltearCarta } from "./motor";
 import { actualizarIntentos } from "./main";
 
-const contenedor = document.getElementById("contenedorCartas");
-if (!contenedor) {
-    console.error("el contenedor de cartas no fue encontrado en el DOM.")
-};
-
 export const actualizarUIcarta = (divCarta: HTMLElement | null, carta: Carta) => {
-    if (!divCarta) {
-        console.error("Error: divCarta no encontrada para la carta", carta);
-        return;
-    }
-    console.log("Actualizando carta", carta);
+    
+    if (divCarta && divCarta instanceof HTMLDivElement) {
+        divCarta.classList.remove("dorso", "volteada", "emparejada");
 
-    divCarta.classList.remove("dorso", "volteada", "emparejada");
-
-    if (carta.emparejada) {
-        divCarta.style.backgroundImage = `url(${carta.imagen})`;
-        divCarta.classList.add("emparejada");
-    } else if (carta.estaVolteada) {
-        divCarta.style.backgroundImage = `url(${carta.imagen})`;
-        divCarta.classList.add("volteada");
+        if (carta.emparejada) {
+            divCarta.style.backgroundImage = `url(${carta.imagen})`;
+            divCarta.classList.add("emparejada");
+        } else if (carta.estaVolteada) {
+            divCarta.style.backgroundImage = `url(${carta.imagen})`;
+            divCarta.classList.add("volteada");
+        } else {
+            divCarta.style.backgroundImage = "";
+            divCarta.classList.add("dorso");
+        }
     } else {
-        divCarta.style.backgroundImage = "";
-        divCarta.classList.add("dorso");
+        console.error("Error: divCarta no encontrada para la carta", carta);
     }
 };
 
 export const mostrarMensajeError = (mensaje: string) => {
     const mensajeError = document.createElement("div");
     mensajeError.textContent = mensaje;
-    mensajeError?.classList.add("mensaje-error");
+    mensajeError.classList.add("mensaje-error");
     document.body.appendChild(mensajeError);
     setTimeout(() => {
-        mensajeError?.remove();
+        mensajeError.remove();
     }, 2000);
 };
 
 export const actualizarUITablero = (tablero: Tablero): void => {
     const contenedorCartas = document.getElementById("contenedorCartas");
-    if (!contenedorCartas) {
-        console.error("Contendor de cartas no encontrado");
-        return;
+    
+    if (contenedorCartas) {
+        contenedorCartas.innerHTML = "";
     }
-    contenedorCartas.innerHTML = "";
+    
+    const contenedor = document.getElementById("contenedorCartas");
+    if (contenedor && contenedor instanceof HTMLDivElement) {
+        tablero.cartas.forEach((carta: Carta, index: number) => {
+            const divCarta = document.createElement("div");
+            divCarta.classList.add("carta", "dorso");
+            divCarta.id = `carta-${index}`;
 
-    tablero.cartas.forEach((carta: Carta, index: number) => {
-        const divCarta = document.createElement("div");
-        divCarta.classList.add("carta", "dorso");
-        divCarta.id = `carta-${index}`;
-
-        divCarta.addEventListener("click", () => {
-            if (sePuedeVoltearCarta(tablero, index)) {
-                voltearCarta(tablero, index);
-                console.log(tablero);
-                actualizarUIcarta(divCarta, tablero.cartas[index]);
-                saberSiEsLaSegundaCarta();
-            } else {
-                console.log('no se puede voltear la carta');
-            }
-            // console.log(`Carta clickeada: índice ${index}`);
-            // if (tablero.estadoPartida !== "partidaCompleta") {
-            //     if(sePuedeVoltearCarta(tablero, index)) {
-            //         voltearCarta(tablero, index,actualizarIntentos);
-            //         actualizarUIcarta(divCarta, tablero.cartas[index])
-            //     }
-            // }
+            divCarta.addEventListener("click", () => {
+                eventoClickCarta(tablero, index, divCarta);
+            });
+            contenedor.appendChild(divCarta);
         });
-        contenedor?.appendChild(divCarta);
-    });
+    } else {
+        console.error("Contenedor de cartas no encontrado");
+    }
 };
 
-const saberSiEsLaSegundaCarta = () => {
+const eventoClickCarta = (tablero: Tablero, indice: number, divCarta: HTMLDivElement) => {
+    if (sePuedeVoltearCarta(tablero, indice)) {
+        voltearCarta(tablero, indice);
+        actualizarUIcarta(divCarta, tablero.cartas[indice]);
+        saberSiEsLaSegundaCarta(tablero);
+    } else {
+        console.log('no se puede voltear la carta');
+    }
+}
+
+const saberSiEsLaSegundaCarta = (tablero: Tablero) => {
     const indiceCartaA = tablero.indiceCartaVolteadaA;
     const indiceCartaB = tablero.indiceCartaVolteadaB;
-    console.log(indiceCartaB, indiceCartaA);
-    if (indiceCartaA !== undefined && indiceCartaB !== undefined) {
-        if (sonPareja(indiceCartaA, indiceCartaB, tablero)) {
-            parejaEncontrada(indiceCartaA, indiceCartaB, tablero);
 
-            if (esPartidaCompleta(tablero)) {
-                console.log('partida completa');
-            }
-        } else {
-            console.log("Al menos llega a aqui");
-            parejaNoEncontrada(tablero, indiceCartaA, indiceCartaB);
-                setTimeout(() => {
-                const divCartaA = document.getElementById(`carta-${indiceCartaA}`);
-                const divCartaB = document.getElementById(`carta-${indiceCartaB}`);
-                actualizarUIcarta(divCartaA, tablero.cartas[indiceCartaA]);
-                actualizarUIcarta(divCartaB, tablero.cartas[indiceCartaB]);
-                actualizarUITablero(tablero);
-            }, 1500);
-        }
+    if (indiceCartaA !== undefined && indiceCartaB !== undefined) {
+        
+        sonPareja(indiceCartaA, indiceCartaB, tablero) ? esPareja(tablero, indiceCartaA, indiceCartaB) : noEsPareja(tablero, indiceCartaA, indiceCartaB);
     }
 };
 
-export const iniciarYActualizarUI = (tablero: Tablero, actualizarIntentos:(intentos: number) => void): void => {
+const esPareja = (tablero: Tablero, indiceCartaA: number, indiceCartaB: number) => {
+    parejaEncontrada(indiceCartaA, indiceCartaB, tablero);
+
+    if (esPartidaCompleta(tablero)) {
+        console.log('partida completa');
+    }
+}
+
+const noEsPareja = (tablero: Tablero, indiceCartaA: number, indiceCartaB: number) => {
+    parejaNoEncontrada(tablero, indiceCartaA, indiceCartaB);
+    setTimeout(() => {
+        const divCartaA = document.getElementById(`carta-${indiceCartaA}`);
+        const divCartaB = document.getElementById(`carta-${indiceCartaB}`);
+        actualizarUIcarta(divCartaA, tablero.cartas[indiceCartaA]);
+        actualizarUIcarta(divCartaB, tablero.cartas[indiceCartaB]);
+    }, 1500);
+    tablero.intentos += 1;
+    actualizarIntentos(tablero.intentos);
+}
+
+export const iniciarYActualizarUI = (tablero: Tablero, actualizarIntentos: (intentos: number) => void): void => {
     inicializarJuego(tablero, actualizarIntentos);
     actualizarUITablero(tablero);
 };
